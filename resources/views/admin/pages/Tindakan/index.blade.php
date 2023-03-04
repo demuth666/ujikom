453b25c29dda8dd0d8401582cd7093fa8c5c9e6c
demuth666 <eevnxxx05@gmail.com>
2023-03-04 14:45:10 +0700

    menyelesaika search

--- resources/views/admin/pages/Tindakan/index.blade.php
+++ resources/views/admin/pages/Tindakan/index.blade.php
@@ -3,10 +3,10 @@
     <main class="table">
         <section class="table__header">
             <h3>Tindakan</h3>
-            <div class="input-group">
-                <input type="search" placeholder="Search Data...">
+            <form action="{{ route('search.tindakan') }}" method="GET" class="input-group">
+                <input type="search" name="data" placeholder="Search Data...">
                 <ion-icon name="search-outline"></ion-icon>
-            </div>
+            </form>
         </section>
         <div class="add">
             <a href={{ route('add.tindakan') }}>
@@ -23,26 +23,30 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($tindakan as $tindakan)
-                        <tr>
-                            <td><strong> {{ $tindakan->nm_tindakan }}</strong></td>
-                            <td> {{ $tindakan->ket }} </td>
-                            <td>
-                                <div class="button-action">
-                                    <a href="{{ route('edit.tindakan', $tindakan->id) }}">
-                                        <button class="button-edit" role="button">Edit</button>
-                                    </a>
-                                    <form action="{{ route('destroy.tindakan', $tindakan->id) }}" id="delete-form"
-                                        method="POST">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button class="button-delete" role="button" type="submit">Hapus</button>
-                                    </form>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforeach
+                    @if (count($tindakan))
+                        @foreach ($tindakan as $tindakan)
+                            <tr>
+                                <td><strong> {{ $tindakan->nm_tindakan }}</strong></td>
+                                <td> {{ $tindakan->ket }} </td>
+                                <td>
+                                    <div class="button-action">
+                                        <a href="{{ route('edit.tindakan', $tindakan->id) }}">
+                                            <button class="button-edit" role="button">Edit</button>
+                                        </a>
+                                        <form action="{{ route('destroy.tindakan', $tindakan->id) }}" id="delete-form"
+                                            method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button class="button-delete" role="button" type="submit">Hapus</button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
                 </tbody>
+            @else
+                <p>Data tidak ditemukan</p>
+                @endif
             </table>
         </section>
     </main>
